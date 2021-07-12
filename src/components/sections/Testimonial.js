ac2436cf3e0d8c950bf8d8f6cc98b3782fe84899
Andre Mosin <andre@mosin.com.br>
2021-07-12 08:15:33 -0700

    fix expansao

--- src/components/sections/Testimonial.js
+++ src/components/sections/Testimonial.js
@@ -62,14 +62,14 @@ const Testimonial = ({
               <div className="tiles-item-inner">
                 <div className="testimonial-item-content">
                   <p className="text-sm mb-0">
-                  A Instruaud com um modelo totalmente inovador e humanizado marca registrada da empresa, irá proporcionar serviços com alto grau de excelência na área de transporte de pacientes e serviços de Home Care aos quatro cantos do Brasil.  
-                      </p>
+                    A Instruaud com um modelo totalmente inovador e humanizado marca registrada da empresa, irá proporcionar serviços com alto grau de excelência na área de transporte de pacientes e serviços de Home Care aos quatro cantos do Brasil.
+                  </p>
                 </div>
                 <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
-                 
+
                   <span className="text-color-low">  </span>
                   <span className="testimonial-item-link">
-                    
+
                   </span>
                 </div>
               </div>
@@ -79,36 +79,36 @@ const Testimonial = ({
               <div className="tiles-item-inner">
                 <div className="testimonial-item-content">
                   <p className="text-sm mb-0">
-                  Serão 26 unidadas sediadas em todas as capitais do Brasil nos proxímos 10 anos
-com ambulâncias e aeronaves novas, equipamentos de última geração e profissionais
-altamente qualificados. A instruaud junto com você Ambassador irá promover uma 
-revolução na área da saúde com a prestação dos melhores serviços de transparte aéreo, terrestre 
-e Home Care do Brasil e quiça do Mundo.
-                      </p>
+                    Serão 26 unidadas sediadas em todas as capitais do Brasil nos proxímos 10 anos
+                    com ambulâncias e aeronaves novas, equipamentos de última geração e profissionais
+                    altamente qualificados. A instruaud junto com você Ambassador irá promover uma
+                    revolução na área da saúde com a prestação dos melhores serviços de transparte aéreo, terrestre
+                    e Home Care do Brasil e quiça do Mundo.
+                  </p>
                 </div>
                 <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
-                
+
                   <span className="text-color-low">  </span>
                   <span className="testimonial-item-link">
-                   
+
                   </span>
                 </div>
               </div>
             </div>
-            
+
 
             <div className="tiles-item reveal-from-left" data-reveal-delay="200">
               <div className="tiles-item-inner">
                 <div className="testimonial-item-content">
                   <p className="text-sm mb-0">
-                  Nossa 1ª unidade de expasão será implantada no coração do Amazonas na capital Manaus considerada a sétima cidade mais populosa do Brasil, e sua região metropolitana, com mais de 2,7 milhões de habitantes, é a 11.ª mais populosa do país. Serão 26 unidades abertas ao longo dos proxímos 10 anos e você terá a chance única de se tornar um Crypto Ambassador e fazer parte de cada uma delas através do token INSTRUAUD - (IAUD).
-                      </p>
-                </div> 
+                    Nossa 1ª unidade de expansão será implantada no coração do Amazonas na capital Manaus considerada a sétima cidade mais populosa do Brasil, e sua região metropolitana, com mais de 2,7 milhões de habitantes, é a 11.ª mais populosa do país. Serão 26 unidades abertas ao longo dos proxímos 10 anos e você terá a chance única de se tornar um Crypto Ambassador e fazer parte de cada uma delas através do token INSTRUAUD - (IAUD).
+                  </p>
+                </div>
                 <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
-                  
+
                   <span className="text-color-low">  </span>
                   <span className="testimonial-item-link">
-                    
+
                   </span>
                 </div>
               </div>
